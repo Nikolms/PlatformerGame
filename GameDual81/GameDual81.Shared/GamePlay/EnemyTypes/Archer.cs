e766f0a5e2da76f639388acdad18adac62c6a9a7
Niko <niko.lms2@gmail.com>
2015-09-08 16:33:43 +0300

    added files

---
+++ GameDual81/GameDual81.Shared/GamePlay/EnemyTypes/Archer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ThielynGame.GamePlay.Actions;
+
+namespace ThielynGame.GamePlay.EnemyTypes
+{
+    class Archer : Enemy
+    {
+        public Archer(Vector2 startPos) : base (startPos)
+        {
+            TextureFileName = "troll_sprite";
+            characterType = "archer";
+
+            MaxHealth = 10;
+            maxSpeedX = 2;
+            acceleration = 0.66f;
+
+            detectionRange = 600;
+            rangePrimaryAttack = 600;
+            rangeSecondaryAttack = 0;
+
+            cooldownBetweenAttacks = 2000;
+            cooldownPrimaryAttack = 2000;
+            cooldownSecondaryAttack = 10000;
+
+            actualSize = new Rectangle(0,0,45,70);
+
+            AI_combatMovement = AI_CombatMovement.Guard;
+            AI_idleMovement = AI_IdleMovement.Patrol;
+
+            setParameters();
+        }
+
+        protected override void DoPrimaryAttack()
+        {
+            
+        }
+
+        public override int GetRangedDamage()
+        {
+            return 20;
+        }
+    }
+}
---
+++ GameDual81/GameDual81.Shared/GamePlay/Projectile.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ThielynGame.GamePlay.StatusEffects;
+
+namespace ThielynGame.GamePlay
+{
+    public enum ProjetileType {Arrow, IceBolt}
+
+    class Projectile : PhysicsObjects, HarmfullObject
+    {
+        int damage;
+        StatusEffect appliedEffect;
+        bool piercing;
+
+        protected Projectile() 
+        {
+            affectedByGravity = false;
+        }
+
+        public static Projectile createProjectile(ProjetileType type, Character actor) 
+        {
+            Projectile P = new Projectile();
+            switch (type) 
+            {
+                case ProjetileType.Arrow:
+                    P = new Projectile() 
+                    { 
+                      velocity = new Vector2(6.5f, 0),
+                      actualSize = new Rectangle(0, 0, 50, 10)
+                    };
+                    break;
+
+                case ProjetileType.IceBolt:
+                    P = new Projectile() 
+                    { 
+                        velocity = new Vector2(4f,0), 
+                        actualSize = new Rectangle(0,0,60,60), 
+                        appliedEffect = StatusEffect.createEffect(actor.level, EffectType.Fragile)
+                    };
+                    break;
+            }
+
+            P.position.X = actor.BoundingBox.Center.X;
+            P.position.Y = actor.BoundingBox.Y + ((actor.BoundingBox.Height - P.actualSize.Height) / 2);
+            P.damage = actor.GetRangedDamage();
+            P.velocity.X = P.velocity.X * (int)actor.Facing;
+            P.alignment = actor.Alignment;
+            return P;
+        }
+        
+
+        public void CheckCollisionWithCharacter(Character C)
+        {
+            if (C.BoundingBox.Intersects(BoundingBox) && alignment != C.Alignment)
+            {
+                C.OnReceiveDamage(damage, false, appliedEffect);
+                IsDead = true;
+            }
+        }
+
+        public override void HandleGroundCollision(collisionCorrection CC, Platform collidedWith)
+        {
+            IsDead = true;
+        }
+
+        public void CollideWithObsticle() 
+        {
+            IsDead = true;
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch S, AnimationFiles.TextureLoader T)
+        {
+            S.Draw(
+                T.GetTexture("TODO"),
+                MyRectangle.AdjustExistingRectangle(BoundingBox), 
+                Color.White);
+        }
+    }
+}
